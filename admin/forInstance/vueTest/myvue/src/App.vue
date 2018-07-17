ec4af9f02aab18f1f9871d70dc5fba4ad9e84d35
chensi1993 <1293614194@qq.com>
2018-07-17 17:59:18 +0800

    项目备份

---
+++ admin/forInstance/vueTest/myvue/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <div class="header">I am header</div> -->
+    <header>I am header</header>
+    <div class="tab">I am tab</div>
+    <div class="content">I am content</div>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+// export default {
+//   name: 'App'
+// }
+
+import header from './components/header/header.vue';
+
+export default{
+  comments: {
+    'v-header': header
+  }
+};
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
